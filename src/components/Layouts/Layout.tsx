d8fd1ed382e29b4b2b83ac0759857ee4892a6ece
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-13 00:35:45 -0300

    feat: refactor Layout component to enhance sidebar item rendering and integrate Calendar

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,15 +1,33 @@
 import * as Tabs from "@radix-ui/react-tabs";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import { NavigationTabs } from "../Tabs";
-import { SecondarySidebarTrigger, SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import {
+  SecondarySidebarTrigger,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "../ui/sidebar";
 import { AppSidebar } from "../Sidebar";
-import { CalendarDotIcon, FileMagnifyingGlassIcon, GraphIcon } from "@phosphor-icons/react";
+import {
+  CalendarDotIcon,
+  FileMagnifyingGlassIcon,
+  GraphIcon,
+} from "@phosphor-icons/react";
+import { Calendar } from "../Calendar";
+import { useState } from "react";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
-const item = [
+interface SidebarItem {
+  title: string;
+  url: string;
+  icon: React.ComponentType;
+}
+
+const item: SidebarItem[] = [
   {
     title: "Inbox",
     url: "#",
@@ -25,14 +43,32 @@ const item = [
     url: "#",
     icon: FileMagnifyingGlassIcon,
   },
-]
+];
 
 export const Layout = ({ children }: LayoutProps) => {
+   const [date, setDate] = useState<Date | undefined>(new Date())
+
   return (
     <Tabs.Root defaultValue="new-tab">
       <Tooltip.Provider>
         <SidebarProvider>
-          <AppSidebar item={item} sidebarGroupLabelText="Application" />
+          <AppSidebar
+            item={
+              <div>
+                {item.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </div>
+            }
+            sidebarGroupLabelText="Application"
+          />
           <div className="w-full bg-zinc-900 h-screen flex flex-col gap-4">
             <div className="flex justify-between px-2 pt-1">
               <div className="flex items-center gap-2">
@@ -43,8 +79,20 @@ export const Layout = ({ children }: LayoutProps) => {
             </div>
             <main className="flex-1 p-4 overflow-y-auto">{children}</main>
           </div>
-           <AppSidebar item={item} sidebarGroupLabelText="Application" sidebarSide="right" />
-        </SidebarProvider>
+          <AppSidebar 
+            item={
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                className="rounded-md border shadow-sm"
+                captionLayout="dropdown"
+              />
+            }
+            sidebarGroupLabelText="Application"
+            sidebarSide="right"
+          />
+        </SidebarProvider >
       </Tooltip.Provider>
     </Tabs.Root>
   );
