be623dd54ff824f343e111342ccfd5c6c1bf4357
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-14 14:16:04 -0300

    feat: update sidebar width constants for improved layout consistency

--- src/utils/constants.ts.ts
+++ src/utils/constants.ts.ts
@@ -1,8 +1,8 @@
 export const SIDEBAR_COOKIE_NAME = "sidebar_state";
 export const SECONDARY_SIDEBAR_COOKIE_NAME = "secondary_sidebar_state";
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-export const SIDEBAR_WIDTH = "16.5rem";
-export const SIDEBAR_WIDTH_MOBILE = "18rem";
-export const SIDEBAR_WIDTH_ICON = "3rem";
+export const SIDEBAR_WIDTH = "270px";
+export const SIDEBAR_WIDTH_MOBILE = "288px";
+export const SIDEBAR_WIDTH_ICON = "48px";
 export const SIDEBAR_KEYBOARD_SHORTCUT = "b";
 export const SECONDARY_SIDEBAR_KEYBOARD_SHORTCUT = "m";
