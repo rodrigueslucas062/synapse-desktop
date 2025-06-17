73bcfb18f9a8bf10da53790e6b7042008c8b9f06
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-16 23:18:09 -0300

    refactor: layout and ServiceSidebar components for improved structure and functionality

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -53,50 +53,51 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <AuthProvider>
       <Tabs.Root defaultValue="new-tab">
-        <ServiceSidebar />
         <Tooltip.Provider>
           <SidebarProvider>
-            <AppSidebar
-              item={
-                <>
-                  {item.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                      <SidebarMenuButton asChild>
-                        <a href={item.url}>
-                          <item.icon />
-                          <span>{item.title}</span>
-                        </a>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </>
-              }
-              sidebarGroupLabelText="Application"
-            />
-            <div className="w-full bg-zinc-900 h-screen flex flex-col gap-4">
-              <div className="flex justify-between px-2 pt-1">
-                <div className="flex items-center gap-2">
-                  <SidebarTrigger />
-                  <NavigationTabs />
+            <div className="flex h-screen w-full bg-zinc-900">
+              {/* <ServiceSidebar /> */}
+              <AppSidebar
+                item={
+                  <>
+                    {item.map((item) => (
+                      <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton asChild>
+                          <a href={item.url}>
+                            <item.icon />
+                            <span>{item.title}</span>
+                          </a>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    ))}
+                  </>
+                }
+                sidebarGroupLabelText="Application"
+              />
+              <div className="w-full flex flex-col gap-4">
+                <div className="left-2 flex justify-between px-2 pt-1">
+                  <div className="left-2 flex items-center gap-2">
+                    <SidebarTrigger />
+                    <NavigationTabs />
+                  </div>
+                  <SecondarySidebarTrigger />
                 </div>
-                <SecondarySidebarTrigger />
+                <main className="flex-1 p-4 overflow-y-auto">{children}</main>
               </div>
-              <main className="flex-1 p-4 overflow-y-auto">{children}</main>
+              <AppSidebar
+                item={
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-md border shadow-sm w-full"
+                    captionLayout="dropdown"
+                  />
+                }
+                sidebarGroupLabelText="Calendário"
+                sidebarSide="right"
+              />
             </div>
-            <AppSidebar
-              item={
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  className="rounded-md border shadow-sm w-full"
-                  captionLayout="dropdown"
-                />
-              }
-              sidebarGroupLabelText="Application"
-              sidebarSide="right"
-            />
-            <SidebarTrigger />
           </SidebarProvider>
         </Tooltip.Provider>
       </Tabs.Root>
--- src/components/Sidebars/ServiceSidebar.tsx
+++ src/components/Sidebars/ServiceSidebar.tsx
@@ -1,98 +1,180 @@
+import * as React from "react";
 import {
-  BrainIcon,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarInput,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import {
   ChalkboardSimpleIcon,
   ListChecksIcon,
   NotepadIcon,
+  BrainIcon,
   SignOutIcon,
+  CommandIcon,
 } from "@phosphor-icons/react";
 import * as Tooltip from "@radix-ui/react-tooltip";
-import { useAuth } from "../Context";
 
-export const ServiceSidebar = () => {
-  const auth = useAuth();
-  const logout = auth ? auth.logout : () => {};
+const navItems = [
+  {
+    href: "/home",
+    icon: <BrainIcon size={20} weight="duotone" />,
+    color: "text-emerald-500",
+    label: "Notion",
+  },
+  {
+    href: "/tasks",
+    icon: <ListChecksIcon size={20} weight="duotone" />,
+    color: "text-purple-500",
+    label: "Tasks",
+  },
+  {
+    href: "/notepad",
+    icon: <NotepadIcon size={20} weight="duotone" />,
+    color: "text-sky-500",
+    label: "Notepad",
+  },
+  {
+    href: "/jamboard",
+    icon: <ChalkboardSimpleIcon size={20} weight="duotone" />,
+    color: "text-orange-500",
+    label: "Jamboard",
+  },
+];
+
+const data = {
+  mails: [
+    {
+      name: "William Smith",
+      email: "williamsmith@example.com",
+      subject: "Meeting Tomorrow",
+      date: "09:34 AM",
+      teaser:
+        "Hi team, just a reminder about our meeting tomorrow at 10 AM.\nPlease come prepared with your project updates.",
+    },
+    {
+      name: "Alice Smith",
+      email: "alicesmith@example.com",
+      subject: "Re: Project Update",
+      date: "Yesterday",
+      teaser:
+        "Thanks for the update. The progress looks great so far.\nLet's schedule a call to discuss the next steps.",
+    },
+  ],
+};
+
+export function ServiceSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  const [activeItem, setActiveItem] = React.useState(navItems[0]);
+  const [mails, setMails] = React.useState(data.mails);
+  const { setOpen } = useSidebar();
 
   return (
-    <Tooltip.Provider>
-      <aside
-        id="default-sidebar"
-        className="fixed top-0 left-0 w-fit h-screen transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar"
+    <Sidebar
+      collapsible="icon"
+      className="overflow-hidden *:data-[sidebar=sidebar]:flex-row"
+      {...props}
+    >
+      <Sidebar
+        collapsible="none"
+        className="w-[calc(var(--sidebar-width-icon)+1px)]! border-r bg-zinc-800"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-zinc-900 flex flex-col">
-          <ul className="space-y-2 font-medium flex-1">
-            {[
-              {
-                href: "/home",
-                icon: <BrainIcon size={20} weight="duotone" />,
-                color: "text-emerald-500",
-                label: "Notion",
-              },
-              {
-                href: "/tasks",
-                icon: <ListChecksIcon size={20} weight="duotone" />,
-                color: "text-purple-500",
-                label: "Tasks",
-              },
-              {
-                href: "/notepad",
-                icon: <NotepadIcon size={20} weight="duotone" />,
-                color: "text-sky-500",
-                label: "Notepad",
-              },
-              {
-                href: "/jamboard",
-                icon: <ChalkboardSimpleIcon size={20} weight="duotone" />,
-                color: "text-orange-500",
-                label: "Jamboard",
-              },
-            ].map((item) => (
-              <li key={item.href}>
+        <SidebarContent className=" min-w-fit py-4">
+          <SidebarGroup>
+            <SidebarGroupContent className="space-y-2">
+              {navItems.map((item) => (
+                <Tooltip.Provider key={item.href}>
+                  <Tooltip.Root>
+                    <Tooltip.Trigger asChild>
+                      <button
+                        onClick={() => {
+                          setActiveItem(item);
+                          setOpen(true);
+                        }}
+                        className={`flex items-center justify-center p-2 rounded-lg bg-zinc-900 hover:bg-zinc-700 transition ${
+                          activeItem.href === item.href ? "bg-zinc-700" : ""
+                        } ${item.color}`}
+                      >
+                        {item.icon}
+                      </button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Portal>
+                      <Tooltip.Content
+                        className="select-none rounded bg-zinc-950 px-3 py-2.5 text-sm leading-none text-white shadow-lg will-change-[transform,opacity] data-[state=delayed-open]:animate-slideRightAndFade"
+                        side="right"
+                        sideOffset={5}
+                      >
+                        {item.label}
+                        <Tooltip.Arrow className="fill-zinc-950" />
+                      </Tooltip.Content>
+                    </Tooltip.Portal>
+                  </Tooltip.Root>
+                </Tooltip.Provider>
+              ))}
+
+              <Tooltip.Provider>
                 <Tooltip.Root>
                   <Tooltip.Trigger asChild>
-                    <a
-                      href={item.href}
-                      className={`flex w-fit items-center justify-center p-2 ${item.color} rounded-lg bg-zinc-800 group`}
+                    <button
+                      onClick={() => console.log("Logout")}
+                      className="flex items-center justify-center p-2 rounded-lg bg-zinc-900 hover:bg-zinc-700 text-red-500 transition"
                     >
-                      {item.icon}
-                    </a>
+                      <SignOutIcon size={20} weight="duotone" />
+                    </button>
                   </Tooltip.Trigger>
                   <Tooltip.Portal>
                     <Tooltip.Content
-                      className="select-none rounded bg-zinc-950 px-[15px] py-2.5 text-[15px] leading-none text-white shadow-lg will-change-[transform,opacity] data-[state=delayed-open]:animate-slideRightAndFade"
+                      className="select-none rounded bg-zinc-950 px-3 py-2.5 text-sm leading-none text-white shadow-lg"
                       side="right"
                       sideOffset={5}
                     >
-                      {item.label}
+                      Logout
                       <Tooltip.Arrow className="fill-zinc-950" />
                     </Tooltip.Content>
                   </Tooltip.Portal>
                 </Tooltip.Root>
-              </li>
-            ))}
-          </ul>
-          <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <button
-              onClick={() => logout()}
-              className="flex w-fit items-center justify-center p-2 text-red-500 rounded-lg bg-zinc-800 group"
-            >
-              <SignOutIcon size={20} weight="duotone" />
-            </button>
-          </Tooltip.Trigger>
-          <Tooltip.Portal>
-            <Tooltip.Content
-              className="select-none rounded bg-zinc-950 px-[15px] py-2.5 text-[15px] leading-none text-white shadow-lg will-change-[transform,opacity] data-[state=delayed-open]:animate-slideRightAndFade"
-              side="right"
-              sideOffset={5}
-            >
-              Logout
-              <Tooltip.Arrow className="fill-zinc-950" />
-            </Tooltip.Content>
-          </Tooltip.Portal>
-        </Tooltip.Root>
-        </div>
-      </aside>
-    </Tooltip.Provider>
+              </Tooltip.Provider>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+
+      <Sidebar collapsible="none" className="hidden flex-1 md:flex bg-zinc-800">
+        <SidebarHeader className="gap-3.5 border-b p-4">
+          <div className="flex w-full items-center justify-between">
+            <div className="text-foreground text-base font-medium">
+              {activeItem?.label}
+            </div>
+          </div>
+          <SidebarInput placeholder="Type to search..." />
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup className="px-0">
+            <SidebarGroupContent>
+              {mails.map((mail) => (
+                <a
+                  href="#"
+                  key={mail.email}
+                  className="hover:bg-sidebar-accent hover:text-sidebar-accent-foreground flex flex-col items-start gap-2 border-b p-4 text-sm leading-tight whitespace-nowrap last:border-b-0"
+                >
+                  <div className="flex w-full items-center gap-2">
+                    <span>{mail.name}</span>
+                    <span className="ml-auto text-xs">{mail.date}</span>
+                  </div>
+                  <span className="font-medium">{mail.subject}</span>
+                  <span className="line-clamp-2 w-[260px] text-xs whitespace-break-spaces">
+                    {mail.teaser}
+                  </span>
+                </a>
+              ))}
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </Sidebar>
   );
-};
+}
