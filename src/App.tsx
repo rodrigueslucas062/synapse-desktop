58ebeb8615f9c8785210e24f66e5a13cc5536f74
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-17 23:50:40 -0300

    refactor: remove commented AuthProvider from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "@radix-ui/themes/styles.css";
 import "./App.css";
 import { Theme } from "@radix-ui/themes";
 import { Synapse } from "./Synapse";
-import { AuthProvider } from "./components/Context";
 import { ThemeProvider } from "./components/Context/themeContext";
 import { Layout } from "./components/Layouts/Layout";
 
@@ -10,11 +9,9 @@ function App() {
   return (
     <Theme appearance="dark">
       <ThemeProvider>
-        {/* <AuthProvider> */}
         <Layout>
           <Synapse />
         </Layout>
-        {/* </AuthProvider> */}
       </ThemeProvider>
     </Theme>
   );
