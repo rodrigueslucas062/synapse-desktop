f6684086bfe9fee8f31e3feb90f0f6cc75c2dd22
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-18 00:12:08 -0300

    feat: refactor NavigationTabs to utilize dynamic tab rendering and closeTab functionality

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,30 +1,49 @@
-import { PlusIcon } from "@phosphor-icons/react";
+import { PlusIcon, X, XIcon } from "@phosphor-icons/react";
 import * as Tabs from "@radix-ui/react-tabs";
 import { CustomTooltip } from "./Tooltip";
+import { useTabs } from "./Context/tabsContext/tabsContext";
 
 export const NavigationTabs = () => {
+  const { tabs, addTab, activeTab, closeTab } = useTabs();
+
   return (
     <Tabs.List className="flex items-center justify-start pt-1.5 px-1 gap-1 text-sm">
-      <CustomTooltip tooltipText="Nova aba" side={"bottom"}>
-        <Tabs.Trigger
-          value="new-tab"
-          className="h-fit px-4 py-1 bg-none text-white rounded-md cursor-pointer
-          hover:bg-zinc-700 transition-colors duration-200
-          data-[state=active]:bg-zinc-800"
-        >
-          Nova aba
-        </Tabs.Trigger>
-      </CustomTooltip>
+      {tabs.map((tab) => {
+        const isActive = activeTab === tab.id;
 
-      <CustomTooltip tooltipText="Nova aba" side={"bottom"}>
-        <Tabs.Trigger
-          value="add-tab"
+        return (
+          <div key={tab.id} className="relative flex items-center">
+            <CustomTooltip tooltipText={tab.label} side="bottom">
+              <Tabs.Trigger
+                value={tab.id}
+                className={`flex items-center min-w-30 justify-between gap-2 px-3 py-1 rounded-md group text-white cursor-pointer transition-colors duration-200
+        ${isActive ? "bg-zinc-800" : "hover:bg-zinc-700"}`}
+              >
+                {tab.label}
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    closeTab(tab.id);
+                  }}
+                  className={`w-4 h-4 flex items-center justify-center text-zinc-400 hover:text-red-500 bg-zinc-900 rounded text-xs transition
+  ${isActive ? "visible" : "invisible group-hover:visible"}`}
+                >
+                  <XIcon size={10} />
+                </button>
+              </Tabs.Trigger>
+            </CustomTooltip>
+          </div>
+        );
+      })}
+
+      <CustomTooltip tooltipText="Nova aba" side="bottom">
+        <button
+          onClick={() => addTab(`tab-${Date.now()}`, "Nova aba")}
           className="h-fit p-2 bg-none text-white rounded-md cursor-pointer
-            hover:bg-zinc-700 transition-colors duration-200
-            data-[state=active]:bg-zinc-900"
+            hover:bg-zinc-700 transition-colors duration-200"
         >
           <PlusIcon size={14} weight="regular" />
-        </Tabs.Trigger>
+        </button>
       </CustomTooltip>
     </Tabs.List>
   );
