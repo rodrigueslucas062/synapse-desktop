d89dcc7782fb530c266fb7e801c3d4875b8e08ea
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-18 00:38:05 -0300

    refactor: encapsulate layout components within ProviderLayout

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,16 +1,10 @@
-import * as Tooltip from "@radix-ui/react-tooltip";
 import { NavigationTabs } from "../Tabs";
-import {
-  SecondarySidebarTrigger,
-  SidebarProvider,
-  SidebarTrigger,
-} from "../ui/sidebar";
+import { SecondarySidebarTrigger, SidebarTrigger } from "../ui/sidebar";
 import { Calendar } from "../Calendar";
 import { useState } from "react";
-import { AuthProvider } from "../Context";
 import { ServiceSidebar } from "../Sidebars/ServiceSidebar";
 import { AppSidebar } from "../Sidebars/Sidebar";
-import { TabsProvider } from "../Context/tabsContext/tabsContext";
+import { ProviderLayout } from "./ProviderLayout";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -20,39 +14,33 @@ export const Layout = ({ children }: LayoutProps) => {
   const [date, setDate] = useState<Date | undefined>(new Date());
 
   return (
-    <AuthProvider>
-      <TabsProvider>
-        <Tooltip.Provider>
-          <SidebarProvider>
-            <div className="flex h-screen w-full bg-zinc-900">
-              <ServiceSidebar />
-              <div className="w-full flex flex-col gap-4">
-                <div className="left-2 flex justify-between px-2 pt-1">
-                  <div className="left-2 flex items-center gap-2">
-                    <SidebarTrigger />
-                    <NavigationTabs />
-                  </div>
-                  <SecondarySidebarTrigger />
-                </div>
-                <main className="flex-1 p-4 overflow-y-auto">{children}</main>
-              </div>
-              <AppSidebar
-                item={
-                  <Calendar
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    className="rounded-md border shadow-sm w-full"
-                    captionLayout="dropdown"
-                  />
-                }
-                sidebarGroupLabelText="Calendário"
-                sidebarSide="right"
-              />
+    <ProviderLayout>
+      <div className="flex h-screen w-full bg-zinc-900">
+        <ServiceSidebar />
+        <div className="w-full flex flex-col gap-4">
+          <div className="left-2 flex justify-between px-2 pt-1">
+            <div className="left-2 flex items-center gap-2">
+              <SidebarTrigger />
+              <NavigationTabs />
             </div>
-          </SidebarProvider>
-        </Tooltip.Provider>
-      </TabsProvider>
-    </AuthProvider>
+            <SecondarySidebarTrigger />
+          </div>
+          <main className="flex-1 p-4 overflow-y-auto">{children}</main>
+        </div>
+        <AppSidebar
+          item={
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              className="rounded-md border shadow-sm w-full"
+              captionLayout="dropdown"
+            />
+          }
+          sidebarGroupLabelText="Calendário"
+          sidebarSide="right"
+        />
+      </div>
+    </ProviderLayout>
   );
 };
