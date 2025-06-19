ff17a22d905fbd48dca69165950abd8dee7d7386
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-19 00:55:17 -0300

    feat: add ToDo component to display general tasks

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,9 @@
+export const ToDo = () => {
+  return (
+    <div className="flex flex-col items-center space-y-4 max-lg:px-4">
+      <h1 className="bg-sky-700 text-white text-sm py-1 px-3 rounded-3xl font-semibold">
+        Tarefas gerais
+      </h1>
+    </div>
+  );
+};
