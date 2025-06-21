93035d79925945f6a6586d6d012f6a58ecfb4c3e
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-21 00:44:10 -0300

    feat: update DialogModal content padding and height

--- src/components/ui/DialogModal.tsx
+++ src/components/ui/DialogModal.tsx
@@ -20,7 +20,7 @@ export const DialogModal = ({
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/70">
-          <Dialog.Content className="fixed z-10 inset-0 md:inset-auto max-md:top-[45%] lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[65vh] bg-zinc-900 rounded-lg flex flex-col outline-none overflow-hidden">
+          <Dialog.Content className="fixed z-10 p-8 inset-0 md:inset-auto max-md:top-[45%] lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[840px] w-full h-[70%] bg-zinc-900 rounded-lg flex flex-col outline-none overflow-hidden">
             <Dialog.Close className="bg-zinc-800 hover:bg-zinc-700 p-2 rounded-full absolute top-2 right-2 text-zinc-200">
               <XIcon size={16} weight="regular" />
             </Dialog.Close>
