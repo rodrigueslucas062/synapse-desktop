c5da4af4d4df7e6e34c6017b90f9b39bd58f78e4
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-21 00:46:53 -0300

    feat: update SIDEBAR_WIDTH to 300px

--- src/utils/constants.ts.ts
+++ src/utils/constants.ts.ts
@@ -1,7 +1,7 @@
 export const SIDEBAR_COOKIE_NAME = "sidebar_state";
 export const SECONDARY_SIDEBAR_COOKIE_NAME = "secondary_sidebar_state";
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-export const SIDEBAR_WIDTH = "270px";
+export const SIDEBAR_WIDTH = "300px";
 export const SIDEBAR_WIDTH_MOBILE = "288px";
 export const SIDEBAR_WIDTH_ICON = "56px";
 export const SIDEBAR_KEYBOARD_SHORTCUT = "b";
