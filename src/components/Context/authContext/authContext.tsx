048463e420bf74ebd9e14d868b70764b1109329b
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-23 00:29:29 -0300

    refactor: formatting and simplifying authentication logic in AuthProvider

--- src/components/Context/authContext/authContext.tsx
+++ src/components/Context/authContext/authContext.tsx
@@ -1,12 +1,12 @@
-import { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import { createContext, useState, useContext, useEffect, ReactNode } from "react";
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
-} from 'firebase/auth';
-import { useNavigate } from 'react-router';
-import { auth } from '../../../utils/firebase';
-
+} from "firebase/auth";
+import { useNavigate } from "react-router";
+import { auth } from "../../../utils/firebase";
+import { load } from "@tauri-apps/plugin-store";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -22,7 +22,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 }
@@ -35,42 +35,57 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, initializeUser);
-    return unsubscribe;
-  }, []);
+    (async () => {
+      const store = await load("auth.dat", { autoSave: true });
+      const localUser = await store.get("user");
 
-  async function initializeUser(user: any) {
-    if (user) {
-      setCurrentUser(user);
-      setIsAuthenticated(true);
-    } else {
-      setCurrentUser(null);
-      setIsAuthenticated(false);
-    }
-    setLoading(false);
-  }
+      console.log("Local user from store:", localUser);
+      if (localUser) {
+        setCurrentUser(localUser);
+      }
+
+      const unsubscribe = onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          const basicInfo = {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          };
+
+          setCurrentUser(basicInfo);
+          setIsAuthenticated(true);
+          await store.set("user", basicInfo);
+        } else {
+          setCurrentUser(null);
+          setIsAuthenticated(false);
+          await store.delete("user");
+        }
+        setLoading(false);
+      });
+
+      return unsubscribe;
+    })();
+  }, []);
 
   const login = async (email: string, password: string) => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      // redireciona se necessário
-      // navigate('/');
     } catch (error: any) {
-      let errorMessage = '';
-      if (error.code === 'auth/invalid-credential') {
-        errorMessage = 'Email ou senha inválidos.';
+      let errorMessage = "";
+      if (error.code === "auth/invalid-credential") {
+        errorMessage = "Email ou senha inválidos.";
       } else {
-        errorMessage = 'Ocorreu um erro ao realizar essa solicitação.';
+        errorMessage = "Ocorreu um erro ao realizar essa solicitação.";
       }
       setError(errorMessage);
-      // toast.error(errorMessage);
     }
   };
 
   const logout = async () => {
     try {
       await signOut(auth);
-      navigate('/login');
+      navigate("/login");
     } catch (error: any) {
       console.error(error.message);
     }
