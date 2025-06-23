5e7e29043965b1cdfed1b74efe76db70712b81ca
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-23 00:09:45 -0300

    feat: add Toaster component to display notifications

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Theme } from "@radix-ui/themes";
 import { Synapse } from "./Synapse";
 import { ThemeProvider } from "./components/Context/themeContext";
 import { Layout } from "./components/Layouts/Layout";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <Theme appearance="dark">
       <ThemeProvider>
         <Layout>
+          <Toaster richColors />
           <Synapse />
         </Layout>
       </ThemeProvider>
