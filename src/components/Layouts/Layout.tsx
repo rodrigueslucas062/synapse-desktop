debcc178c5de6a673be2c6ab1de4e84648ab1d64
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-06-27 18:48:59 -0300

    refactor: reorganize Layout component structure and remove Calendar sidebar

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,10 +1,8 @@
 import { NavigationTabs } from "../Tabs";
 import { SecondarySidebarTrigger, SidebarTrigger } from "../ui/sidebar";
-import { Calendar } from "../Calendar";
-import { useState } from "react";
 import { ServiceSidebar } from "../Sidebars/ServiceSidebar";
-import { AppSidebar } from "../Sidebars/Sidebar";
 import { ProviderLayout } from "./ProviderLayout";
+import { IntegrationSidebar } from "../Sidebars/IntegrationSidebar";
 import { useAutoUpdate } from "@/utils/UpdateChecker";
 
 type LayoutProps = {
@@ -13,36 +11,21 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
   useAutoUpdate();
-  const [date, setDate] = useState<Date | undefined>(new Date());
 
   return (
     <ProviderLayout>
-      <div className="flex h-screen w-full bg-zinc-900 select-none">
-        <ServiceSidebar />
-        <div className="w-full flex flex-col gap-4">
-          <div className="left-2 flex justify-between px-2 pt-1">
-            <div className="left-2 flex items-center gap-2">
-              <SidebarTrigger />
-              <NavigationTabs />
-            </div>
-            <SecondarySidebarTrigger />
+      <ServiceSidebar />
+      <div className="w-full flex flex-col gap-4 select-none">
+        <div className="left-2 flex justify-between px-2 pt-1">
+          <div className="left-2 flex items-center gap-2">
+            <SidebarTrigger />
+            <NavigationTabs />
           </div>
-          <main className="flex-1 p-4 overflow-y-auto">{children}</main>
+          <SecondarySidebarTrigger />
         </div>
-        <AppSidebar
-          item={
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              className="rounded-md border shadow-sm w-full"
-              captionLayout="dropdown"
-            />
-          }
-          sidebarGroupLabelText="Calendário"
-          sidebarSide="right"
-        />
+        <main className="flex-1 p-4 overflow-y-auto">{children}</main>
       </div>
+      <IntegrationSidebar />
     </ProviderLayout>
   );
 };
